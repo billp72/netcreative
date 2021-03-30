d6cfdf6cfaf6e0cb164ac9517c743dc5376eef36
billpope <epop.bill@gmail.com>
2021-03-30 14:24:40 -0400

    added google page tracking

--- src/components/Seo/Seo.js
+++ src/components/Seo/Seo.js
@@ -44,6 +44,14 @@ const Seo = props => {
         name="twitter:creator"
         content={config.authorTwitterAccount ? config.authorTwitterAccount : ""}
       />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-25256758-1"></script>
+      <script>{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-25256758-1');
+      `}</script>
     </Helmet>
   );
 };
